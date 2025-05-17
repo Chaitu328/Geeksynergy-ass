1e487c27f69d34451a55088e4de53d3867a72cb0
chaitu328 <konakallachaitanya@gmail.com>
2025-05-17 17:08:04 +0530

    done

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
+
+require('dotenv').config(); 
 const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
 app.use(express.json());
 
-
-mongoose.connect('mongodb://localhost:27017', {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('MongoDB connection error:', err));
-
 
 const employeeSchema = new mongoose.Schema({
   name: String,
@@ -71,5 +71,5 @@ app.post('/api/products', async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running`);
 });
